42384aed63e1e572143953dc38146ee99cea6b40
Maksym Haravskyy <fechten17@gmail.com>
2020-02-04 18:57:01 +0200

    add a search bar and a table view

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Макс Гаравський on 04.02.2020.
+//  Copyright © 2020 Макс Гаравський. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
